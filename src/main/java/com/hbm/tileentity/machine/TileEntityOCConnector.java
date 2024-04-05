b34edf94ce0ec59c99669dae1cd92e37569645eb
BallOfEnergy <brennan.b.me@gmail.com>
2024-04-05 12:42:39 -0500

    woo

--- src/main/java/com/hbm/tileentity/machine/TileEntityOCConnector.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityOCConnector.java
@@ -21,52 +21,49 @@ public class TileEntityOCConnector extends TileEntityLoadedBase implements Simpl
 	boolean connected = false;
 	String connectedEnum;
 	ManagedPeripheral Peripheral;
-	int time = 0;
 
 	@Override
 	@Optional.Method(modid = "OpenComputers")
 	public void updateEntity() {
 		if (!worldObj.isRemote) {
-			if (time % 10 == 0) {
-				for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
-					Block b = worldObj.getBlock(xCoord + dir.offsetX, yCoord + dir.offsetY, zCoord + dir.offsetZ);
-					if (b instanceof BlockDummyable || b.hasTileEntity(0)) {
-						TileEntity tile;
-						if (b instanceof BlockDummyable) {
-							int[] pos = ((BlockDummyable)b).findCore(worldObj, xCoord + dir.offsetX, yCoord + dir.offsetY, zCoord + dir.offsetZ);
-							if (pos != null) {
-								tile = worldObj.getTileEntity(pos[0], pos[1], pos[2]);
-							} else {
-								MainRegistry.logger.warn("Null value for position of core for block at: " + (xCoord + dir.offsetX) + ", " + (yCoord + dir.offsetY) + ", " + (zCoord + dir.offsetZ));
-								return;
-							}
+			for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
+				Block b = worldObj.getBlock(xCoord + dir.offsetX, yCoord + dir.offsetY, zCoord + dir.offsetZ);
+				if (b instanceof BlockDummyable || b.hasTileEntity(0)) {
+					TileEntity tile;
+					if (b instanceof BlockDummyable) {
+						int[] pos = ((BlockDummyable)b).findCore(worldObj, xCoord + dir.offsetX, yCoord + dir.offsetY, zCoord + dir.offsetZ);
+						if (pos != null) {
+							tile = worldObj.getTileEntity(pos[0], pos[1], pos[2]);
 						} else {
-							tile = worldObj.getTileEntity(xCoord + dir.offsetX, yCoord + dir.offsetY, zCoord + dir.offsetZ);
+							MainRegistry.logger.warn("Null value for position of core for block at: " + (xCoord + dir.offsetX) + ", " + (yCoord + dir.offsetY) + ", " + (zCoord + dir.offsetZ));
+							return;
 						}
-						Class<?> tileClass = tile.getClass();
-						boolean valid = false;
-						for (CompatHandler.Compats c : CompatHandler.Compats.VALID_COMPATS) {
-							if (c.Class.equals(tileClass) || c.Class.equals(tileClass.getSuperclass())) {
-								if (!c.Class.equals(TileEntityOCConnector.class)) { //fuck you
-									valid = true;
-									connected = true;
-									connectedEnum = c.name();
-									Peripheral = (/* bruh */ManagedPeripheral) tile;
-									break;
-								}
+					} else {
+						tile = worldObj.getTileEntity(xCoord + dir.offsetX, yCoord + dir.offsetY, zCoord + dir.offsetZ);
+					}
+					Class<?> tileClass = tile.getClass();
+					boolean valid = false;
+					for (CompatHandler.Compats c : CompatHandler.Compats.VALID_COMPATS) {
+						if (c.Class.equals(tileClass) || c.Class.equals(tileClass.getSuperclass())) {
+							if (!c.Class.equals(TileEntityOCConnector.class)) { //fuck you
+								valid = true;
+								break;
 							}
-						}
-						if(!valid) {
-							connected = false;
-							connectedEnum = null;
-							Peripheral = null;
-							MainRegistry.logger.debug("TE disconnected at block: " + (xCoord + dir.offsetX) + ", " + (yCoord + dir.offsetY) + ", " + (zCoord + dir.offsetZ));
 						}
 					}
+					if (!valid && connected) {
+						connected = false;
+						connectedEnum = null;
+						Peripheral = null;
+						MainRegistry.logger.debug("TE disconnected at block: " + (xCoord + dir.offsetX) + ", " + (yCoord + dir.offsetY) + ", " + (zCoord + dir.offsetZ));
+					} else if (valid && !connected) {
+						connected = true;
+						connectedEnum = c.name();
+						Peripheral = (/* bruh */ManagedPeripheral) tile;
+						MainRegistry.logger.debug("TE connected at block: " + (xCoord + dir.offsetX) + ", " + (yCoord + dir.offsetY) + ", " + (zCoord + dir.offsetZ));
+					}
 				}
-				time = 0;
 			}
-			time++;
 		}
 	}
 
